96a57a7c6c8a70dfc4c9ba9cc5c6e871cb749999
Joe Lin <lin0joe24@gmail.com>
2021-07-09 22:54:07 -0700

    Update Setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
-# contents of setup.py
-import setuptools
+from setuptools import setup, find_packages
 
-PROJECT_NAME = "song_recognition"  # change this!
-
-setuptools.setup(
-    name=PROJECT_NAME,
-    version="1.0",
-    packages=setuptools.find_packages(),
+setup(
+    name="PandaZam",
+    version="0.0.1",
+    author="CogWorks 2021",
+    author_email="",
+    description="Song Recognition Package",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="https://github.com/A-Group-of-Pandas/AudioProject",
+    project_urls={
+        "Bug Tracker": "https://github.com/A-Group-of-Pandas/AudioProjects/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "song_recognition"},
+    packages=find_packages(where="song_recognition"),
+    python_requires=">=3.6",
 )
